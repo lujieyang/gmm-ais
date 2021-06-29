a126c5ea0eaa10da6130fe4cb422fc745cd86938
Lujie Yang <yanglujie@berkeley.edu>
2021-06-29 10:49:35 -0400

    Scale the reward for NN training

--- Experiments/GetTestParameters.py
+++ Experiments/GetTestParameters.py
@@ -65,7 +65,7 @@ def GetTest1Parameters(ncBelief=4, ncAlpha=9, actionScale=2):
     rm = []
     rm.append(GMixture(np.array([-2, -2, -2]), [Gaussian(-21, 1), Gaussian(-19, 1), Gaussian(-17, 1)]))
     rm.append(GMixture(np.array([-2, -2, -2]), [Gaussian(21, 1), Gaussian(19, 1), Gaussian(17, 1)]))
-    rm.append(GMixture(np.array([-10, 2, -10]), [Gaussian(-25, 250), Gaussian(3, 3), Gaussian(25, 250)]))
+    rm.append(GMixture(np.array([-4, 6, -4]), [Gaussian(-25, 250), Gaussian(3, 3), Gaussian(25, 250)]))
     RM = CS_DA_RewardModel(S, A, rm)
 
     # Assemble the POMDP
