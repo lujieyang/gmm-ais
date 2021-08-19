e238e9b50a68cb14f845649b5f0140df873f17e3
Lujie Yang <yanglujie@berkeley.edu>
2021-08-19 12:10:41 -0400

    Make performance plot look nicer

--- src/cluster_sweep.py
+++ src/cluster_sweep.py
@@ -2,13 +2,14 @@ import os
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import csv
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--folder_name", type=str, default="cluster/")
+    parser.add_argument("--folder_name", type=str, default="cluster/parameter3/")
     args = parser.parse_args()
 
     nz_list = [50, 75, 100, 200, 250, 300, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1500]
@@ -21,9 +22,12 @@ if __name__ == '__main__':
         avg_mean.append(np.load(args.folder_name + "mean_{}_{}.npy".format(nz, nb))/10)
         avg_std.append(np.load(args.folder_name + "std_{}_{}.npy".format(nz, nb))/np.sqrt(10))
         dt.append(np.load(args.folder_name + "time_{}_{}.npy".format(nz, nb)))
-    plt.errorbar(nz_list, avg_mean, avg_std, linestyle='None', fmt='-o')
-    plt.xticks(nz_list)
-    plt.legend(loc="best")
+    colors = cm.rainbow(np.linspace(0, 1, len(nz_list)))
+    plt.errorbar(nz_list, avg_mean, avg_std, linestyle='None', fmt='-o', ecolor=colors)
+    # plt.xticks(nz_list)
+    # plt.legend(loc="best")
+    plt.xlabel('Number of AIS')
+    plt.ylabel('Average Return')
     plt.show()
 
     file_name = args.folder_name + "performance.csv"
